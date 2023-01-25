2dac6ef56822589e9fde9431a77f25cd8d8af61f
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-01-25 16:56:31 +0100

    Update App.jsx
    path components in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,9 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
 
-const Home = lazy(() => import('pages/Home'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
 
